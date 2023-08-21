dc69ff605f41ff2df775615955c80fa6e2e273d3
mshoaaaib <110221554+mshoaaaib@users.noreply.github.com>
2023-08-21 12:35:10 +0500

    Update api.js

--- api.js
+++ api.js
@@ -1,20 +1,25 @@
 const axios = require('axios');
 
-const Ticket_NO= 'SCRUM-13' //To be Set before calling the API
+// Accept the ticket number as a command-line argument
+const Ticket_NO = process.argv[2];
 
-const url = 'https://testjiragitautomation.atlassian.net/rest/api/3/issue/'+Ticket_NO;
+if (!Ticket_NO) {
+  console.error('Error: Ticket number not provided.');
+  process.exit(1);
+}
+
+const url = 'https://testjiragitautomation.atlassian.net/rest/api/3/issue/' + Ticket_NO;
 const auth = {
-  
-  //JIRA login email
+  // JIRA login email
   username: 'muhammadshoaibajk@gmail.com',
- 
-  //Token to be copied From JIRA
+
+  // Token to be copied From JIRA
   password: 'ATATT3xFfGF0MsZE1cirJxVkX1B0dSskqdEvUJG0ZeV6PQBxzC7sg22e0rEc_jeaasfMAgoy4gkhYYZ0F8blgp2rKPUD8JUBWZtwwAy5Ihf1Iu-ish7zcggpoJFWisf7IFgkmh3-byFlP7YLWQZYFim1SycJ6fERYj7RWwROn3n8N1hTFeenJ5E=CAA656F8' // Replace with your actual API token
 };
 
 const requestBody = {
   update: {
-    customfield_10037: [  
+    customfield_10037: [
       {
         set: 'Ticket Flagged Successfully'
       }
@@ -22,7 +27,8 @@ const requestBody = {
   }
 };
 
-axios.put(url, requestBody, { auth })
+axios
+  .put(url, requestBody, { auth })
   .then(response => {
     console.log('Response:', response.status);
   })
